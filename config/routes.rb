e5e9f6e07c53d525c078f23603595a56d051f457
YuraTarasenko <tarasenkoum@gmail.com>
2018-03-20 17:37:29 +0200

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  resources :posts
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+
+  get 'page/index'
+  root 'page#index'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
